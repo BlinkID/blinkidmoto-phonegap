6da089df2642dbc7bc7912a921e90d8efe082863
Jurica Cerovec <jurica.cerovec@microblink.com>
2018-04-20 09:54:26 +0200

    Added check if VIN has digits in first three places

--- BlinkMoto/src/ios/sources/Overlay/PPOcrOverlayViewController.m
+++ BlinkMoto/src/ios/sources/Overlay/PPOcrOverlayViewController.m
@@ -207,11 +207,25 @@ static NSString * const kLicensePlateOcrParser = @"License Plate OCR Parser";
     }
 }
 
+- (BOOL)string:(NSString *)string hasDigitsOnIndexes:(NSArray<NSNumber *> *)indexes {
+    for (NSNumber *index in indexes) {
+        if (isdigit([string characterAtIndex:[index intValue]])) {
+            return true;
+        }
+    }
+    return false;
+}
+
 - (BOOL)isVinStandard:(NSString *)vin {
     unichar c = [vin characterAtIndex:0];
 
-    if (c == 'I' || c == '1') {
-        NSLog(@"Vin starts with I or 1");
+    if (c == 'I') {
+        NSLog(@"Vin starts with I");
+        return NO;
+    }
+
+    if ([self string:vin hasDigitsOnIndexes:@[@(0), @(1), @(2)]]) {
+        NSLog(@"VIN has digits in first three places!");
         return NO;
     }
 
